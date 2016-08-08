a87ab4971a100b667b75df599b685b430dbbf55a
Matt Rosanio <mmrosani@syr.edu>
2016-08-08 10:55:41 -0700

    Add about page component

---
+++ app/about-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'about-page',
+  template: `
+    <div class="about-page">
+      <h3>About</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a est et augue ornare cursus eu nec leo. Praesent eu tempor augue. Aenean orci justo, feugiat at lectus et, luctus feugiat quam. Integer in tempor nunc. Aliquam rutrum vulputate turpis at interdum. Curabitur pretium, urna iaculis placerat lacinia, risus sem scelerisque dolor, sed pellentesque arcu risus sed ante. Nunc suscipit ex mi, eu dapibus leo posuere ac. Maecenas maximus posuere fermentum. Vivamus sem tortor, tincidunt et feugiat placerat, dapibus sed felis. Praesent sagittis lacus vel diam finibus, a interdum nisl ultrices. In hac habitasse platea dictumst. Sed consequat, magna at ultrices maximus, ipsum nisi vehicula dui, ac pulvinar nisl neque ut tortor. Quisque euismod dolor in dolor lobortis, eget venenatis ex elementum. Ut ut elit sed ex tincidunt hendrerit id ac sem. Phasellus nec sapien sit amet felis elementum scelerisque vel laoreet ex.</p>
+    </div>
+  `
+})
+
+export class AboutPageComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from 'angular2/core';
 import {NavViewComponent} from './nav.component';
 import {HomePageComponent} from './home-page.component';
 import {RoomsPageComponent} from './rooms-page.component';
+import {AboutPageComponent} from './about-page.component';
 
 @Component({
   selector: 'my-app',
-  directives: [NavViewComponent, HomePageComponent, RoomsPageComponent],
+  directives: [NavViewComponent, HomePageComponent, RoomsPageComponent, AboutPageComponent],
   template: `
     <nav-view (onNavItemSelect)="navItemWasSelected($event)"></nav-view>
     <h1 id="title">Melissa's Guest House</h1>
     <hr id="title-border">
     <home-page *ngIf="currentPage==='Home'"></home-page>
     <rooms-page *ngIf="currentPage==='Rooms'"></rooms-page>
+    <about-page *ngIf="currentPage==='About'"></about-page>
   `
 })
 
@@ -23,5 +25,6 @@ export class AppComponent {
   navItemWasSelected(selectedNavItem: String) {
     this.currentPage = selectedNavItem;
     console.log(this.currentPage);
+    scrollTo(0, 0);
   }
 }
