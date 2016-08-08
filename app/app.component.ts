79a7b950846c963de97d8324530e61a99057479a
Matt Rosanio <mmrosani@syr.edu>
2016-08-08 10:49:18 -0700

    add a rooms component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from 'angular2/core';
 import {NavViewComponent} from './nav.component';
 import {HomePageComponent} from './home-page.component';
+import {RoomsPageComponent} from './rooms-page.component';
 
 @Component({
   selector: 'my-app',
-  directives: [NavViewComponent, HomePageComponent],
+  directives: [NavViewComponent, HomePageComponent, RoomsPageComponent],
   template: `
     <nav-view (onNavItemSelect)="navItemWasSelected($event)"></nav-view>
     <h1 id="title">Melissa's Guest House</h1>
     <hr id="title-border">
     <home-page *ngIf="currentPage==='Home'"></home-page>
+    <rooms-page *ngIf="currentPage==='Rooms'"></rooms-page>
   `
 })
 
