8cb29d58c7a8221bfe795c235721bc564caec2c9
Matt Rosanio <mmrosani@syr.edu>
2016-08-08 10:25:49 -0700

    Add functionality to click on different nav links and have home page show or hide accordingly

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,12 +6,20 @@ import {HomePageComponent} from './home-page.component';
   selector: 'my-app',
   directives: [NavViewComponent, HomePageComponent],
   template: `
-    <nav-view></nav-view>
+    <nav-view (onNavItemSelect)="navItemWasSelected($event)"></nav-view>
     <h1 id="title">Melissa's Guest House</h1>
-    <home-page></home-page>
+    <hr id="title-border">
+    <home-page *ngIf="currentPage==='Home'"></home-page>
   `
 })
 
 export class AppComponent {
-
+  public currentPage: String;
+  constructor() {
+    this.currentPage = "Home";
+  }
+  navItemWasSelected(selectedNavItem: String) {
+    this.currentPage = selectedNavItem;
+    console.log(this.currentPage);
+  }
 }
--- app/nav.component.ts
+++ app/nav.component.ts
@@ -1,22 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component, EventEmitter} from 'angular2/core';
 
 @Component({
   selector: 'nav-view',
+  outputs: ['onNavItemSelect'],
   template: `
     <div class="nav-view">
-      <div class="nav-item">
+      <div class="nav-item" (click)="navItemSelected('Home')">
         <img src="../resources/images/ic_home_white_18dp_2x.png" class="nav-icon"/>
         <p>Home<p>
       </div>
-      <div class="nav-item">
+      <div class="nav-item" (click)="navItemSelected('Rooms')">
         <img src="../resources/images/ic_hotel_white_18dp_2x.png" class="nav-icon"/>
         <p>Rooms<p>
       </div>
-      <div class="nav-item">
+      <div class="nav-item" (click)="navItemSelected('About')">
         <img src="../resources/images/ic_help_white_18dp_2x.png" class="nav-icon"/>
         <p>About<p>
       </div>
-      <div class="nav-item">
+      <div class="nav-item" (click)="navItemSelected('Contact')">
         <img src="../resources/images/ic_email_white_18dp_2x.png" class="nav-icon"/>
         <p>Contact<p>
       </div>
@@ -25,5 +26,12 @@ import {Component} from 'angular2/core';
 })
 
 export class NavViewComponent {
-
+  public onNavItemSelect: EventEmitter<String>;
+  constructor() {
+    this.onNavItemSelect = new EventEmitter();
+  }
+  public navItemSelected(selectedItem: String) {
+    console.log(selectedItem);
+    this.onNavItemSelect.emit(selectedItem);
+  }
 }
